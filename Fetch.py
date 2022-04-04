e4a477bd11a0f37b22dc07751509f1fc0b6ae474
Rob Thayer <theblack_apple@ymail.com>
2022-04-04 14:48:20 -0400

    Add project files.

---
+++ Fetch.py
@@ -0,0 +1,49 @@
+import requests
+from lib.GetTokenClass import ParseConfig,GetAuthToken
+import json
+import sys
+
+cp = ParseConfig(r'./config/config.ini', 'URL', 'conoFetchUrl')
+searchSigItemUrl = cp.configfileparam
+
+# Fetchwhere decorator function
+def apireq_decorator(func):
+    def apireqdecorator_fn(*args,**kwargs):
+        token,request = func(*args,**kwargs)
+        searchHeaders = {
+                        'accept': 'application/json; charset=utf-8', 
+                        'Content-Type': 'application/json;charset=utf-8', 
+                        'Authorization': 'Bearer ' + token
+                        }
+        idm_search_response = requests.post(searchSigItemUrl, 
+                                        headers=searchHeaders, 
+                                        data=request, 
+                                        verify=False)
+        fetchWhereResponse = json.loads(idm_search_response.content)
+        prettyfetchWhereResponse = json.dumps(fetchWhereResponse, indent=4, sort_keys=True)
+        
+        print("Enter a path and folder to save the file: ")
+        folderinput = input()
+        print("Enter a filename: ")
+        print("Note: File names will be saved with the .json extension")
+        fileinput  = input()
+        with open(folderinput+fileinput+".json", "w", encoding="utf-8") as jsonresponse:
+            jsonresponse.write(prettyfetchWhereResponse)
+                             
+    return apireqdecorator_fn   
+
+@apireq_decorator
+def fetchRequest(tkn,payload):
+    return tkn,payload 
+
+def run(call):
+    fetch = GetAuthToken()
+    run = fetchRequest(fetch.auth_token,fetch.processPayload(call))
+    return run    
+
+def main():
+    inputarg = sys.argv[1]
+    run(inputarg)    
+
+if __name__==main():
+    main()     
---
+++ lib/GetTokenClass.py
@@ -0,0 +1,45 @@
+import configparser
+import json
+import requests
+import urllib3
+
+class ParseConfig(object):
+    def __init__(self, configfile,section,sectionval):
+        self.configParserObj = configparser.RawConfigParser()
+        __configParser = self.configParserObj
+        __configParser.read(configfile)
+        self.configfileparam = __configParser.get(section,sectionval)
+        
+class GetAuthUrl(ParseConfig):
+    def __init__(self):
+        super().__init__(r'./config/config.ini', 'Params', 'ionapi_file')
+
+    def processPayload(self,payloadfile):
+        with open("./payloads/"+payloadfile+".json", "r")as payload:
+            payloadrequest = payload.read()
+        return payloadrequest        
+
+    def parseionapi(tokenurl,_tokenurl):
+        with open(GetAuthUrl().configfileparam, "r") as readionapi:
+            __authkeys = json.load(readionapi)
+
+        def getcredentials(user,password,client_id,client_secret):
+            urllib3.disable_warnings()
+            data = {
+                    'grant_type': 'password',
+                    'username': __authkeys[user], 
+                    'password': __authkeys[password]
+                    }
+            authrequest = requests.post(__authkeys[tokenurl]+__authkeys[_tokenurl],
+                          data=data, verify=False, allow_redirects=False, 
+                          auth=(__authkeys[client_id],__authkeys[client_secret]))
+            authtoken = json.loads(authrequest.content)
+            return authtoken['access_token']
+        return getcredentials('saak','sask','ci','cs')
+
+class GetAuthToken(GetAuthUrl):
+    def __init__(self):
+        self.auth_token = GetAuthToken.parseionapi('pu','ot')
+
+
+
